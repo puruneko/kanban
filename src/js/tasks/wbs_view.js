cc2effab77125473a2be20fc6a94efb5e26b0b1b
puruneko <prog.puru.neko.cat@gmail.com>
2019-02-21 07:47:01 +0900

    electron-build [bug]

--- src/js/tasks/wbs_view.js
+++ src/js/tasks/wbs_view.js
@@ -2,7 +2,6 @@ import React from 'react'
 //import {ContextMenu, MenuItem, ContextMenuTrigger} from 'react-contextmenu'
 import WBSAction from './WBS_action'
 import WBSStore from './WBS_store'
-import jstree from 'jstree'
 
 export default class WBS extends React.Component {
 
